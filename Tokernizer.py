d7ad7ae5637d499e6653529af6db9e001841d94e
ShanthishaShyamana <shanthisha2002@gmail.com>
2025-06-01 02:26:13 +0530

    Implement CSE Machine and Stack Data Structure
    - Added `stack.py` to define a Stack class with methods for push, pop, and checking if the stack is empty.
    - Introduced `csemachine.py` to implement the core functionality of the CSE machine, including control structure generation and built-in function handling.
    - Created `structure.py` to define data structures such as Delta, Tau, Lambda, and Eta for managing control structures.
    - Enhanced the Stack class to support indexing and debugging representation.
    - Implemented error handling for empty stack scenarios in the CSE machine.

--- Tokernizer.py
+++ Tokernizer.py
@@ -3,6 +3,8 @@ import re
 from dataclasses import dataclass
 from parser import Parser
 from standerdizer import standardize
+from ASTnode import ASTnode
+from csemachine import run_cse_machine
 
 @dataclass
 class Token:
@@ -92,6 +94,11 @@ def main():
     st = standardize(stack[-1])
 
     print_st(st)
+
+    # print("Running CSE machine...")
+    result = run_cse_machine(st)
+
+    print("Final Output:", result)
 
 
 def print_ast(node, indent=0):
